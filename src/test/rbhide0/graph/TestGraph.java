6c3b25c62d36e1d7358a75a557acb532d9962bb6
Ravi Bhide <rbhide+git@gmail.com>
2011-11-11 13:36:17 -0800

    Added test cases for Graph.

--- src/test/rbhide0/graph/TestGraph.java
+++ src/test/rbhide0/graph/TestGraph.java
@@ -2,45 +2,12 @@ package rbhide0.graph;
 
 import junit.framework.TestCase;
 
+import java.util.Set;
+
 /**
  * @author Ravi Bhide
  */
 public class TestGraph extends TestCase {
-    // Test implementation of a Vertex.
-    public static class DefaultVertex implements Vertex {
-        private String _label;
-
-        public DefaultVertex(String label) {
-            _label = label;
-        }
-
-        public Object getLabel() {
-            return _label;
-        }
-
-        public Object getSatelliteData() {
-            return this;
-        }
-
-        public boolean equals(Object v) {
-            boolean equals = false;
-            if (v instanceof DefaultVertex) {
-                if (v != null) {
-                    if (_label == null) {
-                        equals = (((DefaultVertex) v)._label == null);
-                    } else {
-                        equals = _label.equals(((DefaultVertex) v)._label);
-                    }
-                }
-            }
-            return equals;
-        }
-
-        public int hashCode() {
-            return _label == null ? 0 : _label.hashCode();
-        }
-    }
-
     public void test1() {
         // Create graph.
         Graph graph = new Graph();
@@ -80,6 +47,32 @@ public class TestGraph extends TestCase {
         assertTrue(graph.containsEdge(v1, v5));
         assertTrue(graph.containsEdge(v2, v5));
         assertFalse(graph.containsEdge(v2, v3));
+
+        // Test getEdges() call for vertex v1.
+        Set<Vertex> edges1 = graph.getEdges(v1);
+        assertEquals(4, edges1.size());
+        assertTrue(edges1.contains(v2));
+        assertTrue(edges1.contains(v3));
+        assertTrue(edges1.contains(v4));
+        assertTrue(edges1.contains(v5));
+        // Test getEdges() call for vertex v2.
+        Set<Vertex> edges2 = graph.getEdges(v2);
+        assertEquals(2, edges2.size());
+        assertTrue(edges2.contains(v1));
+        assertTrue(edges2.contains(v5));
+        // Test getEdges() call for vertex v3.
+        Set<Vertex> edges3 = graph.getEdges(v3);
+        assertEquals(1, edges3.size());
+        assertTrue(edges3.contains(v1));
+        // Test getEdges() call for vertex v4.
+        Set<Vertex> edges4 = graph.getEdges(v4);
+        assertEquals(1, edges4.size());
+        assertTrue(edges4.contains(v1));
+        // Test getEdges() call for vertex v5.
+        Set<Vertex> edges5 = graph.getEdges(v5);
+        assertEquals(2, edges5.size());
+        assertTrue(edges5.contains(v1));
+        assertTrue(edges5.contains(v2));
 
         // Test statistics.
         assertEquals(5, graph.getNumVertices());
